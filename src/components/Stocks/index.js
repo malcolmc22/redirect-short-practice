845783fb6d4f759e1e2b65cdea623c7ee0ab0615
Malcolm Caleb <138073963+malcolmc22@users.noreply.github.com>
2023-10-10 18:41:20 -0400

    Initial commit

---
+++ src/components/Stocks/index.js
@@ -0,0 +1,16 @@
+import { Redirect } from "react-router-dom";
+
+function Stocks() {
+  const loggedIn = false;
+
+  if (!loggedIn) {
+    return <Redirect to="not-logged-in" />;
+  }
+  return (
+    <div className="comp orange">
+      <h1>Stocks Component</h1>
+    </div>
+  );
+}
+
+export default Stocks;
